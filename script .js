44452619e55d90f7aabe3872d1749d56295eeed6
Sumit Mishra <sm0141136@gmail.com>
2024-08-13 11:41:21 +0530

    complit project animaction

---
+++ script .js
@@ -0,0 +1,28 @@
+var tl=gsap.timeline()
+
+tl.from(".nav h3",{
+    y:-50,
+    opacity:0,
+    delay:0.8,
+    stagger:1
+})
+
+tl.from(".main h1",{
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4
+})
+tl.from("img",{
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:0.5,
+    stagger:0.4,
+})
+tl.from(".nav-part-3",{
+    x:-500,
+    opacity:0,
+    delay:0.8,
+    stagger:0.4
+})
